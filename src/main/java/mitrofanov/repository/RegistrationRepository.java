3d7394780fc0871e0bf74a9e304b3e04f23bb911
AllyMw <dmitry.m228@mail.ru>
2024-01-13 15:41:00 +0300

    pet

---
+++ src/main/java/mitrofanov/repository/RegistrationRepository.java
@@ -0,0 +1,67 @@
+package mitrofanov.repository;
+
+import lombok.SneakyThrows;
+import mitrofanov.entity.User;
+import mitrofanov.model.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class RegistrationRepository {
+    DBConnection dbConnection;
+    public boolean addUser(User user) {
+        try {
+            Connection connection = DBConnection.getConnection();
+
+            // Готовим запрос для вставки нового пользователя
+            String query = "INSERT INTO player (chatid, gold, power, agility, mastery, weight, fightingpower) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setLong(1, user.getChatId());
+            statement.setLong(2, user.getGold());
+            statement.setInt(3, user.getPower());
+            statement.setInt(4, user.getAgility());
+            statement.setInt(5, user.getMastery());
+            statement.setInt(6, user.getWeight());
+            statement.setLong(7, user.getFightingPower());
+
+            // Выполняем запрос
+            int rowsInserted = statement.executeUpdate();
+
+            // Закрываем соединение с базой данных
+            statement.close();
+            connection.close();
+
+            // Если запрос выполнен успешно, возвращаем true
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @SneakyThrows
+    public void setNickNamebyChatId(String nickName, Long chatId) {
+        Connection connection = DBConnection.getConnection();
+        String sql = "UPDATE player SET nickname = ? WHERE chatid = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, nickName);
+        statement.setLong(2, chatId);
+        statement.executeUpdate();
+        statement.close();
+        connection.close();
+    }
+
+    @SneakyThrows
+    public void setRaceByChatId(String race, Long chatId) {
+        Connection connection = DBConnection.getConnection();
+        String sql = "UPDATE player SET race = ? WHERE chatid = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, race);
+        statement.setLong(2, chatId);
+        statement.executeUpdate();
+        statement.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/mitrofanov/repository/StatusRepository.java
@@ -0,0 +1,71 @@
+package mitrofanov.repository;
+
+import lombok.SneakyThrows;
+import mitrofanov.model.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StatusRepository {
+    DBConnection dbConnection;
+    public int getStatusByChatId(Long chatId) {
+        Integer status = null;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String sql = "SELECT statuscode FROM status WHERE chatid = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setLong(1, chatId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                status = resultSet.getInt("statuscode");
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+    public void addNewChatID(Long chatId) {
+        try {
+            Connection connection = DBConnection.getConnection();
+            String sql = "INSERT INTO status (chatid, statuscode) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setLong(1, chatId);
+            statement.setInt(2, 1);
+            statement.executeQuery();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SneakyThrows
+    public boolean hasChatId(Long chatId) { // true если пользователь есть
+
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT COUNT(*) FROM status WHERE chatid = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setLong(1, chatId);
+            ResultSet resultSet = statement.executeQuery();
+            int count = 0;
+            if(resultSet.next()){
+                 count = resultSet.getInt(1);
+            }
+            return count > 0;
+    }
+    @SneakyThrows
+    public void insertStatusCode(Long chatId, int stCode) {
+        Connection connection = DBConnection.getConnection();
+        String sql = "UPDATE status SET statuscode = ? WHERE chatid = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, stCode);
+        statement.setLong(2, chatId);
+        statement.executeUpdate();
+        statement.close();
+        connection.close();
+    }
+}
